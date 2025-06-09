599d913e4d8303f506bae4e7419c2d68d9f1872d
agusn16 <agusbenitez157@gmail.com>
2025-06-09 06:32:40 -0300

    agregando el POST

--- main.py
+++ main.py
@@ -25,7 +25,12 @@ app.add_middleware(
 # Simula Base de datos
 mensajes_db = []
 
-
+# Crear un nuevo mensaje 
+@app.post("/mensajes", response_model=Mensaje)
+def crear_mensaje(mensaje: Mensaje):
+    mensaje.id = len(mensajes_db) + 1
+    mensajes_db.append(mensaje)
+    return mensaje
 
 
 
